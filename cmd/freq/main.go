29d770f49feca349ae23b65c7dac84ee539d9668
farukkandemir <farukkandemir09@gmail.com>
2026-02-06 21:03:50 -0500

    added jam command with track struct

--- cmd/freq/main.go
+++ cmd/freq/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"path/filepath"
 	"time"
@@ -11,6 +14,18 @@ import (
 	"github.com/gopxl/beep/v2/mp3"
 	"github.com/gopxl/beep/v2/speaker"
 )
+
+type JamendoTrack struct {
+	Name       string `json:"name"`
+	ArtistName string `json:"artist_name"`
+	Audio      string `json:"audio"`
+}
+
+type JamendoResponse struct {
+	Results []JamendoTrack `json:"results"`
+}
+
+var baseUrl string = "https://api.jamendo.com/v3.0/tracks/?client_id=6e1ba05b&format=jsonpretty&limit=1"
 
 func main() {
 
@@ -25,6 +40,44 @@ func main() {
 	command := os.Args[1]
 
 	switch command {
+	case "jam":
+
+		if len(os.Args) < 3 {
+			fmt.Println("Please provide a command for music(e.g freq jam chill)")
+			return
+		}
+
+		songType := os.Args[2]
+
+		fullPath := baseUrl + "&" + songType
+
+		resp, err := http.Get(fullPath)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		body, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		var tracks JamendoResponse
+
+		err = json.Unmarshal(body, &tracks)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for _, track := range tracks.Results {
+			fmt.Println(track)
+		}
+
 	case "local":
 
 		if len(os.Args) < 3 {
